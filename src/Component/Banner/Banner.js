b8a01e53bcb1470f8ef0892fb36203f92b68f077
DynamoAbir <99138761+DynamoAbir@users.noreply.github.com>
2023-04-01 05:55:28 +0600

    Banner Added

---
+++ src/Component/Banner/Banner.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import image from '../../Assets/quiz-2.jpg'
+
+const Banner = () => {
+    return (
+        <div className="hero  bg-base-200">
+            <div className="hero-content flex-col lg:flex-row">
+                <img src={`${image}`} className="max-w-sm rounded-lg shadow-2xl" />
+                <div className='mx-20'>
+                    <h1 className="mb-5 text-5xl font-bold ">Welcome to WeβQuiz</h1>
+                    <h3 className="mb-5 text-3xl">Lets test your Programming Skill</h3>
+                    <p className="mb-5 text-2x">Here you can test your skills on React,JavaScript,CSS and Git</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+
+export default Banner;
